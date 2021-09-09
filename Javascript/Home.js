85a9d501719d6b567eb64fc0363322b5b90b51c7
Arthur <arthur.saunier2@gmail.com>
2021-09-09 14:43:47 +0200

    smull stuff

--- Javascript/Home.js
+++ Javascript/Home.js
@@ -1,6 +1,6 @@
 var partie1 = "<div class='fond'><div class='affichage'><input type='button' value='×' onclick='cacher();' class='btn-fermer btn-outline-secondary'>";
  
-var partie2 = "<input type='text' class='champ' autocomplete='off' placeholder='Utilisateur'><input type='text' class='champ' autocomplete='off' placeholder='Mot de passe'>";
+var partie2 = "<input type='text' class='champ form-control' autocomplete='off' placeholder='Utilisateur'><input type='text' class='champ form-control' autocomplete='off' placeholder='Mot de passe'>";
  
 var partie3 = "<br><br><input  type='submit' class='btn btn-secondary btn-sm border-white bg-white text-black' value='connexion'> <input  type='submit' class='btn btn-secondary btn-sm border-white bg-white text-black' value='inscription'></div></div>";
  
