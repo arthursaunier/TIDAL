79d181ef9481e181f0f9094a0d261cd4b09ed4e3
Arthur <arthur.saunier2@gmail.com>
2021-09-09 17:16:10 +0200

    add list on patho page

--- Javascript/Home.js
+++ Javascript/Home.js
@@ -3,7 +3,6 @@ var partie1 = "<div class='fond'><div class='affichage'><input type='button' val
 var partie2 = "<input type='text' class='champ form-control' autocomplete='off' placeholder='Utilisateur'><input type='text' class='champ form-control' autocomplete='off' placeholder='Mot de passe'>";
  
 var partie3 = "<br><br><input  type='submit' class='btn btn-secondary btn-sm border-white bg-white text-black' value='connexion'> <input  type='submit' class='btn btn-secondary btn-sm border-white bg-white text-black' value='inscription'></div></div>";
- 
 
 function afficher() {
 document.getElementById("auth").innerHTML= partie1 + partie2 + partie3;
@@ -12,6 +11,3 @@ document.getElementById("auth").innerHTML= partie1 + partie2 + partie3;
 function cacher(){
 document.getElementById("auth").innerHTML="";
 }
-
-
-
