9b633f9a28f1566d28a927f3c8e61bfb170d2b90
Arthur <arthur.saunier2@gmail.com>
2021-09-30 15:20:07 +0200

    Add display on .php files

--- view/frontend/pathologies.php
+++ view/frontend/pathologies.php
@@ -3,5 +3,5 @@ define('SMARTY_DIR', '/home/web/TIDAL/public/smarty-3.1.39/libs/');
 require_once(SMARTY_DIR . 'Smarty.class.php');
 $smarty = new Smarty();
 
-
+$smarty->display('template/pathologies.tpl');
 ?>
