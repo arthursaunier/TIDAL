81e992d43056c2921bcc5a19ef1f03a4ed512b59
Arthur <arthur.saunier2@gmail.com>
2021-10-13 09:13:21 +0200

    include once instead of require (test)

--- controller/frontend.php
+++ controller/frontend.php
@@ -4,20 +4,20 @@ require('frontend.php');
 
 function home()
 {
-    require('../view/frontend/home.php');
+    include_once('../view/frontend/home.php');
 }
 
 function info()
 {
-    require('../view/frontend/info.php');
+    include_once('../view/frontend/info.php');
 }
 
 function pathologies()
 {
-    require('../view/frontend/pathologies.php');
+    include_once('../view/frontend/pathologies.php');
 }
 function signup()
 {
-    require('../view/frontend/signup.php');
+    include_once('../view/frontend/signup.php');
 }
 
