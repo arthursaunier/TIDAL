1b7a9eb011fd54472f0b715189d51054083102ae
Arthur <arthur.saunier2@gmail.com>
2021-10-13 09:29:31 +0200

    (test) fix path

--- view/frontend/home.php
+++ view/frontend/home.php
@@ -3,4 +3,4 @@ define('SMARTY_DIR', '/home/web/TIDAL/public/smarty-3.1.39/libs/');
 require_once(SMARTY_DIR . 'Smarty.class.php');
 $smarty = new Smarty();
 
-$smarty->display('template/home.tpl');
+$smarty->display('TIDAL/template/home.tpl');
