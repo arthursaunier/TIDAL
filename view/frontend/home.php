3bc1248d9f6e0a332f194cebb9045021eeb38cf2
Arthur <arthur.saunier2@gmail.com>
2021-10-14 14:35:33 +0200

    fix smarty path for VM

--- view/frontend/home.php
+++ view/frontend/home.php
@@ -1,5 +1,5 @@
 <?php
-define('SMARTY_DIR', '/home/web/TIDAL/public/smarty-3.1.39/libs/');
+define('SMARTY_DIR', '/home/web/Documents/TIDAL/public/smarty-3.1.39/libs/');
 require_once(SMARTY_DIR . 'Smarty.class.php');
 $smarty = new Smarty();
 
