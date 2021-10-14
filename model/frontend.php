fd55afa8725de5ec676da58b51eb430280037d82
Theo <theobruendet@gmail.com>
2021-10-14 17:05:03 +0200

    rowcount

--- model/frontend.php
+++ model/frontend.php
@@ -10,7 +10,7 @@ function getList()
     FROM SymptPatho AS SP
     INNER JOIN patho AS P ON P.idP = SP.idP 
     INNER JOIN meridien AS M ON M.code = P.mer) TabI ON TabI.idS = S.idS');
-    $num_results = $req->num_rows;
+    $num_results = $req->rowCount();
     for ($i ; $i<$num_results ; $i++){
         $rows[] = $req->fetch_assoc(); 
     }
@@ -22,7 +22,7 @@ function getFilterPatho()
 {
     $conn = dbConnect();
     $req = $conn->query('SELECT P.desc AS pathologie FROM patho AS P');
-    $num_results = $req->num_rows;
+    $num_results = $req->rowCount();
     for ($i ; $i<$num_results ; $i++){
         $rowspatho[] = $req->fetch_assoc(); 
     }
@@ -34,7 +34,7 @@ function getFilterMeridien()
 {
     $conn = dbConnect();
     $req = $conn->query('SELECT M.nom AS meridien FROM meridien AS M');
-    $num_results = $req->num_rows;
+    $num_results = $req->rowCount();
     for ($i ; $i<$num_results ; $i++){
         $rowsmer[] = $req->fetch_assoc(); 
     }
