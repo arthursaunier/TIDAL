15c4feb3b1fa270291b276d3a0791ce4c90c6e7a
Theo <theobruendet@gmail.com>
2021-11-08 16:18:13 +0100

    d

--- model/frontend.php
+++ model/frontend.php
@@ -15,6 +15,7 @@ function getList()
     INNER JOIN meridien AS M ON M.code = P.mer) TabI ON TabI.idS = S.idS');
     $req->execute();
     $data=$req->fetchAll(PDO::FETCH_ASSOC);
+    print_r($data);
     $smarty->assign('reqlist',$data);
 }
 
