2d494974391b103e3632bb6b33ff649a641d197c
Theo <theobruendet@gmail.com>
2021-11-08 15:39:15 +0100

    f

--- model/frontend.php
+++ model/frontend.php
@@ -5,19 +5,14 @@ function getList()
 {
     $i=0;
     $conn = dbConnect();
-    $req = $conn->query('SELECT S.desc symptome, TabI.desc patho, TabI.nom meridien 
+    $req = $conn->prepare('SELECT S.desc symptome, TabI.desc patho, TabI.nom meridien 
     FROM symptome AS S
     INNER JOIN 
     (SELECT SP.idS, P.desc, M.nom
     FROM SymptPatho AS SP
     INNER JOIN patho AS P ON P.idP = SP.idP 
     INNER JOIN meridien AS M ON M.code = P.mer) TabI ON TabI.idS = S.idS');
-    $num_results = $req->rowCount();
-    for ($i ; $i<$num_results ; $i++){
-        $rows[] = $req->fetch(PDO::FETCH_ASSOC); 
-    }
-    $smarty->assign('rows',$rows);
-    return $rows;
+    $smarty->assign('reqlist',$req);
 }
 
 function getFilterPatho()
@@ -25,12 +20,9 @@ function getFilterPatho()
     $i=0;
     $conn = dbConnect();
     $req = $conn->query('SELECT P.desc AS pathologie FROM patho AS P');
-    $num_results = $req->rowCount();
-    for ($i ; $i<$num_results ; $i++){
-        $rowspatho[] = $req->fetch(PDO::FETCH_ASSOC); 
-    }
-    $smarty->assign('rowspatho',$rowspatho);
-    return $rowspatho;
+
+    $smarty->assign('reqfiltre',$req);
+    
 }
 
 function getFilterMeridien()
@@ -38,12 +30,8 @@ function getFilterMeridien()
     $i=0;
     $conn = dbConnect();
     $req = $conn->query('SELECT M.nom AS meridien FROM meridien AS M');
-    $num_results = $req->rowCount();
-    for ($i ; $i<$num_results ; $i++){
-        $rowsmer[] = $req->fetch(PDO::FETCH_ASSOC); 
-    }
-    $smarty->assign('rowsmer',$rowsmer);
-    return $rowsmer;
+    $smarty->assign('reqmer',$req);
+    
 }
 
 
