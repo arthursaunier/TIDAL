9982ed217b3a6495a91bce0acf76deed5ad235a3
Arthur <arthur.saunier2@gmail.com>
2021-11-09 21:09:53 +0100

    tre

--- public/Javascript/Home.js
+++ public/Javascript/Home.js
@@ -2,7 +2,7 @@ var partie1 = "<form method='POST' action='index.php'><div class='fond '><div cl
  
 var partie2 = "<input name='email' type='text' class='champ form-control' autocomplete='off' placeholder='Utilisateur'><input name='password' type='password' class='champ form-control' autocomplete='off' placeholder='Mot de passe'>";
  
-var partie3 = "<br><br><input  type='submit' class='btn btn-secondary btn-sm border-white bg-white text-black' value='connexion'> <input  type='submit' class='btn btn-secondary btn-sm border-white bg-white text-black' value='inscription'></div></div></form>";
+var partie3 = "<br><br><input  type='submit' class='btn btn-secondary btn-sm border-white bg-white text-black' value='connexion'> <input  type='button' onclick='signUp()' class='btn btn-secondary btn-sm border-white bg-white text-black' value='inscription'></div></div></form>";
 
 function afficher() {
 document.getElementById("auth").innerHTML= partie1 + partie2 + partie3;
@@ -10,4 +10,8 @@ document.getElementById("auth").innerHTML= partie1 + partie2 + partie3;
   
 function cacher(){
 document.getElementById("auth").innerHTML="";
+}
+
+function signUp(){
+    location.href = "index.php?action=signup";
 }
