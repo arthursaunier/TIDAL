db4637ee342e0a4a3e61c0f59506e2822cefd315
Theo <theobruendet@gmail.com>
2021-11-10 15:14:28 +0100

    d

--- model/frontend.php
+++ model/frontend.php
@@ -65,6 +65,7 @@ function getListefilterMer()
     INNER JOIN patho AS P ON P.idP = SP.idP 
     INNER JOIN meridien AS M ON M.code = P.mer) TabI ON TabI.idS = S.idS WHERE meridien = ?');
     $req->execute(array($mer));
+    print_r($req);
     while($elem=$req->fetch(PDO::FETCH_ASSOC)){
         array_push($data, $elem);
     }
